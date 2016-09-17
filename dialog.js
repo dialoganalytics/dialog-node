7a5688f7752e10593cd31c120eebc999d998243e
Philippe Dionne <dionne.phil@gmail.com>
2016-09-17 10:36:55 -0400

    Add default user agent

--- dialog.js
+++ dialog.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var request = require("request");
+var defaultUserAgent = require("./user_agent");
 
 module.exports = {
 
   track: function(apiToken, botId, payload) {
     var options = {
       url: 'https://api.dialoganalytics.com/v1/b/' + botId + '/track?token=' + apiToken,
+      headers: {
+        'User-Agent': defaultUserAgent
+      },
       json: payload
     }
 
