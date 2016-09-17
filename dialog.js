bc4f1deb156cbe0265bc2ad26d3bc57356171108
Philippe Dionne <dionne.phil@gmail.com>
2016-09-17 09:08:17 -0400

    Refactor post request

--- dialog.js
+++ dialog.js
@@ -5,8 +5,18 @@ var request = require("request");
 module.exports = {
 
   track: function(apiToken, botId, payload) {
-    var endpoint = 'https://api.dialoganalytics.com/v1/b/' + botId + '/track?token=' + apiToken;
+    var options = {
+      url: 'https://api.dialoganalytics.com/v1/b/' + botId + '/track',
+      headers: {
+        'HTTP_AUTHORIZATION': apiToken
+      },
+      json: payload
+    }
 
-    return request({ method: 'POST', uri: endpoint, json: payload });
+    function callback(error, response, body) {
+        console.log(JSON.parse(body));
+    };
+
+    return request.post(options, callback);
   }
 };
