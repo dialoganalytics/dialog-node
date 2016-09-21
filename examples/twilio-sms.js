e400d07c1f87ee3a28d23314cdce13b1bed744f1
Philippe Dionne <dionne.phil@gmail.com>
2016-09-21 15:29:41 -0400

    Add blank examples for alexa and twilio sms

---
+++ examples/twilio-sms.js
@@ -0,0 +1,10 @@
+// This is an example integration of dialog-node with Twilio Programmable SMS
+//
+// Get started:
+//   1. Create an account on https://app.dialoganalytics.com
+//   2. Find your API token in your personal settings page and your Bot ID in the bot's settings page
+//   3. Clone this repository and run `npm install`
+//   4. Replace hardcoded API tokens, username and bot ID values
+//   5. When developping locally use a service like ngrok.com to expose a server running on your machine. For example: `ngrok http 8080`
+//   6. Run this example by typing `node examples/alexa.js' from the repository root
+//   7. Read more on how to make the most out of the possibilities offered by Dialog here: https://docs.dialoganalytics.com
